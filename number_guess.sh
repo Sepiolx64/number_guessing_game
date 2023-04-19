f5f78be535558c3ff4c450b35cb58a81b95cb500
Sepiolx64 <62660052+Sepiolx64@users.noreply.github.com>
2023-04-20 01:56:07 +0200

    Update number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
-echo "Enter your username:"
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+READ_USERNAME() {
+echo -e "Enter your username:"
 read USERNAME
+
+if [[ -z $USERNAME ]]
+then
+  READ_USERNAME
+else
+  CHECK_USERNAME=$($PSQL "SELECT username FROM users WHERE username='$USERNAME'")
+  if [[ -z $CHECK_USERNAME ]]
+  then
+    INSERT_USER=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
+    echo -e "Welcome $USERNAME! It looks like this is your first time here."
+  else
+    USER_INFO=$($PSQL "SELECT username, COALESCE(MAX(game_id),0) AS games_played, COALESCE(MIN(number_of_guesses),0) AS best_game FROM users LEFT JOIN games ON users.user_id=games.user_id WHERE username='$USERNAME' GROUP BY username")
+    echo $USER_INFO | while IFS="|" read USERNAME GAMES_PLAYED BEST_GAME
+    do
+      echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+    done
+  fi
+fi
+}
+
+RANDOM_NUMBER=$[$RANDOM % 1000 + 1]
+READ_USERNAME
